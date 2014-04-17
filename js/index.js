1d034e6a532b62d06e6eff2457ecb75d1df94f3e
Dylandy Chang <steven80919@gmail.com>
2014-04-17 12:59:40 +0800

    finish page2 start page3

--- js/index.js
+++ js/index.js
@@ -1,7 +1,30 @@
+var click_and_show = function( origin , change ){
+  $(origin).click(function(){
+    $(change).show();
+  });
+}
+var timer = function(i){
+  $('.time-block').html(i);
+}
+var clear_interval = function(){
+  
+}
+
 jQuery(function($){
   $('#start-button').click(function(){
-    $('#index-page').fadeOut('slow');
-    $('#second-page').fadeIn('fast');
+    $('#index-page').fadeOut('fast');
+    $('#second-page').fadeIn(3000);
+  });
+  $('.banner').click(function(){
+    $(this).fadeOut(800);
   });
 
+  click_and_show( '#tutorial-banner' , '#setsumei01' );
+  click_and_show( '#setsumei01' , '#third-page');
+  
+  $('#third-page').click(function(){
+    for( var i = 0; i < 10 ; i++){
+      setInterval( timer(i) , 1000 );
+    }
+  });
 });
