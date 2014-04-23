d17dfc4726038a96ee1bf3dc26f2b56d591b33a1
Dylandy Chang <steven80919@gmail.com>
2014-04-23 15:01:33 +0800

    change something

--- js/index.js
+++ js/index.js
@@ -10,7 +10,6 @@ var click_and_show = function( origin , change ){
     $(change).show();
   });
 }
-
 /* the page three*/
 var fourty_in_ten = function(){
   $('.time-block')[0].innerHTML = 0;
@@ -33,6 +32,16 @@ var fourty_in_ten = function(){
   });
 }
 
+var random_in_ten = function(n){
+  var the_top = Math.floor(Math.random()*(100-0));
+  var the_left = Math.floor(Math.random()*(100-0));
+  $('#mov-button').css('top', the_top+'%');
+  $('#mov-button').css('left', the_left+'%');
+  if( n < 10 ){
+    setTimeout( random_in_ten(++n) , 1000 );
+  }
+}
+
 /* main function */
 jQuery(function($){
   $('#start-button').click(function(){
@@ -46,13 +55,18 @@ jQuery(function($){
   click_and_show( '#tutorial-banner' , '#setsumei01' );
   click_and_show( '#setsumei01' , '#third-page');
   click_and_show( '#to_page4' , '#fourth-page' );
+  click_and_show( '#fourth-page' , '#fifth-page' );
   
   $('#to_page4').click(function(){
     $('#third-page').remove();
     $('#fourth-page').show();
+    $('#setsumei02').fadeIn(800);
   });
   /* page three*/
   $('#setsumei01').click( fourty_in_ten );
   $('.restart-fast').click( fourty_in_ten );
   $('.explore').click(function(){window.open('','_self').close();}); // explore would close it the app itself
+
+  /*page five*/
+  $('#setsumei02').click(random_in_ten(0)); 
 });
